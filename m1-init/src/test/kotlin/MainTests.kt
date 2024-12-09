6a2214cc868eb00e37da71017341313e033b452e
Olesya Ivashkevich <olesya.ivashkevich@spbpu.com>
2024-12-09 17:22:12 +0300

    m1-init

---
+++ m1-init/src/test/kotlin/MainTests.kt
@@ -0,0 +1,10 @@
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class MainTests {
+
+    @Test
+    fun firstTest() {
+        assertTrue(true)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "recipe-book"
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+include("m1-init")
